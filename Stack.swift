a722b09d9148e36ed599581000a19781d11f4a50
NeilsUltimateLab <neilsultimatelab@icloud.com>
2016-12-25 14:32:00 +0530

    Swift stack for the Stack Operations

---
+++ Stack.swift
@@ -0,0 +1,39 @@
+//
+//  Stack.swift
+//  
+//
+//  Created by Neil on 18/12/16.
+//
+//
+
+import Foundation
+
+public struct Stack<T> {
+    
+    fileprivate var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    public func peek() -> T? {
+        return array.last
+    }
+    
+    public mutating func trash() {
+        array.removaAll()
+    }
+    
+}
